e6a34027e6c233ac248976be41e42c668e76a6d9
RSik88888 <romain.siekaniec@gmail.com>
2020-05-11 23:32:11 +0200

    boutton dans tabscore

--- src/scrabble_1/TabScore.java
+++ src/scrabble_1/TabScore.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class TabScore extends JPanel{
@@ -22,6 +23,29 @@ public class TabScore extends JPanel{
 		this.modl=m;
 		this.ctrl=C;
 		this.repaint();
+		
+		int sizebutonW =Interface.iWidth/4;
+		int sizebutonH=Interface.iHeight/5;
+		
+		//partie boutons
+	    JButton BEnd = new JButton("Valider");
+	    JButton BNext = new JButton("Passer");
+	    JButton BPioche = new JButton("Changer");
+	    JButton Bsupp = new JButton("Annuler");
+		
+		BEnd.setBounds(0,Interface.iHeight/2 ,sizebutonW,sizebutonH);
+		BEnd.setText("VALIDER MOT");
+		BNext.setBounds(Interface.iWidth*1/4 +sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
+		BNext.setText("FIN DU TOUR");
+		BPioche.setBounds(Interface.iWidth*2/4 + 2*sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
+		BPioche.setText("CHANGER LETTRE.S");
+		Bsupp.setBounds(Interface.iWidth*3/4 + 3*sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
+		Bsupp.setText("ANNULER");
+		
+		this.add(BEnd);
+		this.add(BNext);
+		this.add(BPioche);
+		this.add(Bsupp);
 	}
 	
 	public void drawTabScore(Graphics g) {
