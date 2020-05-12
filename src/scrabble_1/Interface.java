ea12a825d11e2817964a884938821c584bd30540
Monkeekee <matthieu180198@gmail.com>
2020-05-12 02:21:37 +0200

    RangeBtn

--- src/scrabble_1/Interface.java
+++ src/scrabble_1/Interface.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 
 
@@ -32,7 +33,7 @@ private static final long serialVersionUID = 1L;
 	public static int iHeight=Vue.screenHeight;
 	
 	public Main main;//pour les jetons du joueur
-	
+	public RangeBtn Rb;
 	public TabScore Tb;
 	
 	public Modele modl;
@@ -51,33 +52,12 @@ private static final long serialVersionUID = 1L;
 		
 		this.main = new Main(m,c);
 		this.Tb = new TabScore(m,c);
-		
-		int sizebutonW =Interface.iWidth/5;
-		int sizebutonH=Interface.iHeight/5;
-		
-		//partie boutons
-	    JButton BEnd = new JButton("Valider");
-	    JButton BNext = new JButton("Passer");
-	    JButton BPioche = new JButton("Changer");
-	    JButton Bsupp = new JButton("Annuler");
-	    
-	   
-		//BEnd.setBounds(0,Interface.iHeight/2 ,sizebutonW,sizebutonH);
-		BEnd.setText("VALIDER MOT");
-		//BNext.setBounds(sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
-		BNext.setText("FIN DU TOUR");
-		//BPioche.setBounds(2*sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
-		BPioche.setText("CHANGER LETTRE.S");
-		//Bsupp.setBounds(3*sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
-		Bsupp.setText("ANNULER");
-		
-		this.add(BEnd,BorderLayout.CENTER);
-		this.add(BNext,BorderLayout.EAST);
-		this.add(BPioche,BorderLayout.EAST);
-		this.add(Bsupp,BorderLayout.EAST);
-		
+		this.Rb = new RangeBtn(m,c);
+
+		this.add(Rb,BorderLayout.CENTER);
 		this.add(main,BorderLayout.SOUTH);
 		this.add(Tb,BorderLayout.NORTH);
+		
 		
 		this.setVisible(true);
 		
@@ -144,6 +124,49 @@ private static final long serialVersionUID = 1L;
 			
 		}
 		
+	}
+	
+	public class RangeBtn extends JPanel{
+		//**
+		private static final long serialVersionUID = 1L;
+		public Modele modl;
+		public Controleur ctrl;
+		
+		
+		
+		public RangeBtn(Modele m, Controleur C) {
+			super();
+			this.setVisible(true);
+			this.setPreferredSize(new Dimension(Interface.iWidth,Interface.iHeight/3));
+			this.modl=m;
+			this.ctrl=C;
+			
+			this.setLayout(new SpringLayout());
+			
+			int sizebutonW =Interface.iWidth/5;
+			int sizebutonH=Interface.iHeight/5;
+			
+			//partie boutons
+		    JButton BEnd = new JButton("Valider");
+		    JButton BNext = new JButton("Passer");
+		    JButton BPioche = new JButton("Changer");
+		    JButton Bsupp = new JButton("Annuler");
+		    
+		   
+			//BEnd.setBounds(0,Interface.iHeight/2 ,sizebutonW,sizebutonH);
+			BEnd.setText("VALIDER MOT");
+			//BNext.setBounds(sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
+			BNext.setText("FIN DU TOUR");
+			//BPioche.setBounds(2*sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
+			BPioche.setText("CHANGER LETTRE.S");
+			//Bsupp.setBounds(3*sizebutonW,Interface.iHeight/2,sizebutonW,sizebutonH);
+			Bsupp.setText("ANNULER");
+			
+			this.add(BEnd);
+			this.add(BNext);
+			this.add(BPioche);
+			this.add(Bsupp);
+		}
 	}
 	
 	
